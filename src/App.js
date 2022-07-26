630eab81ec9d257c6aa15abb774e6e8d44734866
John N. Norman <normanj777@gmail.com>
2022-07-26 16:25:27 -0400

    EOD

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="component-container"></div>
+    </div>
+  );
+};
+
+export default App;
