4472264c8e2d113ac8d2bb0b64ef4b2433968334
John N. Norman <normanj777@gmail.com>
2022-07-27 08:42:56 -0400

    reset

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 const App = () => {
   return (
     <div className="app-container">
-      <div className="component-container"></div>
+      <div className="component-container">
+        <p>hello world</p>
+      </div>
     </div>
   );
 };
